cd330bdd09c8d407b1e6214d82592347439c326a
anbraten <anton@ju60.de>
2021-01-11 17:55:13 +0000

    Deploying to master from  @ 9269ecad00136b04807a171a6a4138b9a0904e26 🚀

--- _assets/OAuthCallback.66f05dfd.js
+++
@@ -1,1 +0,0 @@
-import{d as e,u as a,g as s,o as n,c as r}from"./index.e0ee3544.js";var t=e({name:"OAuthCallback",setup(){const e=a();s((()=>{e.push({name:"home"})}))}});t.render=function(e,a,s,t,o,u){return n(),r("div")};export default t;
