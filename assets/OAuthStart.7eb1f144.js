6750199b86a728483637a097ade32af6599e505b
julianhahn <40185075+julianhahn@users.noreply.github.com>
2021-01-28 12:19:28 +0000

    Deploying to master from  @ 5f7e17fc33ec51dcafd2d5fb13f23587d1d9593c 🚀

--- assets/OAuthStart.7eb1f144.js
+++
@@ -1,1 +0,0 @@
-import{d as t,b as r,a as o,c as e}from"./index.05bb8384.js";const a=t({name:"OAuthStart",props:{oauthProvider:{type:String,required:!0}},setup(t){r((()=>{location.href=`http://localhost:3030/oauth/${t.oauthProvider}`}))}});a.render=function(t,r,a,i,n,s){return o(),e("div")};export default a;
