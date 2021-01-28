99fbbae9ec65bd1ff63a686a8270d0b18aaf7e44
anbraten <anton@ju60.de>
2021-01-28 12:54:26 +0000

    Deploying to master from  @ 48c38f0f2aaa361cac484114156ec0843a29ed28 🚀

--- assets/LabeledInput.4a09b050.js
+++
@@ -1,1 +0,0 @@
-import{d as e,a as l,c as a,h as s,t,j as n}from"./index.980530d0.js";var r=e({name:"LabeledInput",props:{label:{type:String,required:!0}}});const d={class:"labeled-input w-full flex flex-col"},o={class:"label font-bold ml-6 text-white"};r.render=function(e,r,i,p,f,u){return l(),a("div",d,[s("span",o,t(e.label),1),n(e.$slots,"default")])};export{r as _};
