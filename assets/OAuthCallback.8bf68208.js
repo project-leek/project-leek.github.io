28282c94f4a9d0c898f16af6c216e0c5b14cf7a9
anbraten <anton@ju60.de>
2021-02-03 21:19:44 +0000

    Deploying to master from  @ 95cd2732f52b48dc4c90851ebfeda07e51cf3fe7 🚀

---
+++ assets/OAuthCallback.8bf68208.js
@@ -0,0 +1,1 @@
+import{d as a,u as e,o as s,a as n,c as t}from"./index.2a861458.js";const o=a({name:"OAuthCallback",setup(){const a=e();s((()=>{a.push({name:"home"})}))}});o.render=function(a,e,s,o,r,u){return n(),t("div")};export default o;
