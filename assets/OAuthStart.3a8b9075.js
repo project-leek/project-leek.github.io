cba2027c710b5e170dd5743a5e800f76161ff1ef
anbraten <anton@ju60.de>
2021-02-03 16:04:10 +0000

    Deploying to master from  @ 8280e3a76853e19e75a317e35348a2f25c63e7d3 🚀

---
+++ assets/OAuthStart.3a8b9075.js
@@ -0,0 +1,1 @@
+import{d as t,b as r,a as e,c as o}from"./index.70d9e76a.js";const a=t({name:"OAuthStart",props:{oauthProvider:{type:String,required:!0}},setup(t){r((()=>{location.href=`http://localhost:3030/oauth/${t.oauthProvider}`}))}});a.render=function(t,r,a,d,i,n){return e(),o("div")};export default a;
