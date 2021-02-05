2ff230564189338b1df18918c7cbbfab1867d58a
tcoro <9517685+tcoro@users.noreply.github.com>
2021-02-05 22:01:07 +0000

    Deploying to master from  @ bd1a0f901e494e27da6ddbea8b7b81296dcb79e6 🚀

--- assets/LabeledInput.cb5ac39a.js
+++
@@ -1,1 +0,0 @@
-import{d as e,a as l,c as a,g as s,t,i as n}from"./index.05f65000.js";var r=e({name:"LabeledInput",props:{label:{type:String,required:!0}}});const o={class:"labeled-input w-full flex flex-col"},d={class:"label font-bold ml-6 text-white"};r.render=function(e,r,i,f,p,u){return l(),a("div",o,[s("span",d,t(e.label),1),n(e.$slots,"default")])};export{r as _};
