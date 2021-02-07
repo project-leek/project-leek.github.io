32f3322357c2527e8eca44fa43b1b6b22cbac79a
anbraten <anton@ju60.de>
2021-02-07 22:06:03 +0000

    Deploying to master from  @ 821b25f41fb275dd25f78b44ec543c0769ddb931 🚀

--- assets/OAuthStart.08fb7a4e.js
+++
@@ -1,1 +0,0 @@
-import{d as t,b as r,a as o,c as e}from"./index.28282cd4.js";const a=t({name:"OAuthStart",props:{oauthProvider:{type:String,required:!0}},setup(t){r((()=>{location.href=`http://localhost:3030/oauth/${t.oauthProvider}`}))}});a.render=function(t,r,a,d,i,n){return o(),e("div")};export default a;
