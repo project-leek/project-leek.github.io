e048095ddff815f3431b656f40f58a5bda66fc8d
anbraten <anton@ju60.de>
2021-02-08 22:06:27 +0000

    Deploying to master from  @ 30fc700f8cbdd079e787c947ab53d68c7a1910db 🚀

---
+++ assets/LabeledInput.559ef639.js
@@ -0,0 +1,1 @@
+import{d as e,a,c as l,g as s,t,i as n}from"./index.cd9c5a38.js";var r=e({name:"LabeledInput",props:{label:{type:String,required:!0}}});const d={class:"labeled-input w-full flex flex-col"},o={class:"label font-bold ml-6 text-white"};r.render=function(e,r,i,c,p,f){return a(),l("div",d,[s("span",o,t(e.label),1),n(e.$slots,"default")])};export{r as _};
