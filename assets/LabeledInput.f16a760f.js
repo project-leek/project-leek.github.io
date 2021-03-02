a0c59f200c03fc266b589086514e2211666173d3
anbraten <anton@ju60.de>
2021-03-02 20:48:04 +0000

    Deploying to master from @ project-leek/project-leek@dc0e85560a8e0a1ade6a28a13da1814a5223c09d 🚀

--- assets/LabeledInput.f16a760f.js
+++
@@ -1,1 +0,0 @@
-import{d as a,a as e,c as l,j as s,t,l as n}from"./index.101acba7.js";var r=a({name:"LabeledInput",props:{label:{type:String,required:!0}}});const o={class:"labeled-input w-full flex flex-col"},d={class:"label font-bold ml-6 text-white"};r.render=function(a,r,i,p,b,c){return e(),l("div",o,[s("span",d,t(a.label),1),n(a.$slots,"default")])};export{r as _};
