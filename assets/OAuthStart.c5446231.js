80996433319fd54ae3f42d61728b231dd7cfd4b0
julianhahn <40185075+julianhahn@users.noreply.github.com>
2021-03-03 21:26:53 +0000

    Deploying to master from @ project-leek/project-leek@eae2a3c094803bfba661a9ea67d9a2d46146536f 🚀

---
+++ assets/OAuthStart.c5446231.js
@@ -0,0 +1,1 @@
+import{d as r,b as e,e as t,a,c as o}from"./index.3488b203.js";const u=r({name:"OAuthStart",props:{oauthProvider:{type:String,required:!0}},setup(r){e((()=>{location.href=`${t.value}/oauth/${r.oauthProvider}?redirect=auth/callback`}))}});u.render=function(r,e,t,u,i,d){return a(),o("div")};export default u;
