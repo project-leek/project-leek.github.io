4e691263c7aa7301f336bf82eb582a8add06bb02
anbraten <anton@ju60.de>
2021-03-04 18:08:06 +0000

    Deploying to master from @ project-leek/project-leek@6260de59da43ca19f4107532abd5229b09885cde 🚀

---
+++ assets/OAuthStart.2ef3f140.js
@@ -0,0 +1,1 @@
+import{d as r,b as a,e,a as t,c as o}from"./index.4ac08c93.js";const u=r({name:"OAuthStart",props:{oauthProvider:{type:String,required:!0}},setup(r){a((()=>{location.href=`${e.value}/oauth/${r.oauthProvider}?redirect=auth/callback`}))}});u.render=function(r,a,e,u,i,c){return t(),o("div")};export default u;
